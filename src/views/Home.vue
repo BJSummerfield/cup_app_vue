e4d0182a04c14b63d3147ccad2e6dea49665f2d1
Brian Summerfield <brianjsummerfield@gmail.com>
2020-02-14 10:27:44 -0600

    added files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <hr>
+    <div v-for="cup in cups">
+      <h2> {{ cup.material }}</h2>
+      <h2> {{ cup.size }}</h2>
+      <h2> {{ cup.color }}</h2>
+      <hr>
+    </div>
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Cups!",
+      cups: []
+    };
+  },
+  created: function() {
+    axios.get("/api/cups").then(response => {
+      this.cups = response.data;
+    });
+  },
+  methods: {}
+};
 </script>
